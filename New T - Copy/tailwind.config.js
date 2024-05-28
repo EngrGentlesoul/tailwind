8a3f35d93553d9b1d98603c82182ef9d86f97705
EngrGentlesoul <151466848+EngrGentlesoul@users.noreply.github.com>
2024-05-28 17:24:18 +0100

    Add files via upload

---
+++ New T - Copy/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1448px"
+    },
+    extend: {
+      colors: {
+        brightRed: "hsl(12, 88%, 59%)",
+        brightRedLight: "hsl(12, 88%, 69%)",
+        brightRedSupLight: "hsl(12, 88%, 69%)",
+        darkBlue: "hsl(228, 39%, 23%)",
+        darkGrayishBlue: "hsl(227, 12%, 61%)",
+        veryDarkBlue: "hsl(233, 12%, 13%)",
+        veryPalakRed: "hsl(13, 100%, 96%)",
+        veryLightkGray: "hsl(0, 0%, 98%)",
+      }
+    },
+  },
+  plugins: [],
+}
+
